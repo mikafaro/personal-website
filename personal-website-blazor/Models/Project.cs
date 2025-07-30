55184e8d3c4f2f902cd4158ba5705a6d0b37509e
Mikael Færøvik <mikael.farovik@gmail.com>
2025-07-30 16:54:41 +0200

    Changed Role to ProjectEffortType (enum)

--- personal-website-blazor/Models/Project.cs
+++ personal-website-blazor/Models/Project.cs
@@ -7,9 +7,15 @@ namespace PersonalWebsiteBlazor.Models
         public required string Name { get; set; }
         public required int Year { get; set; }
         public required List<string> Technologies { get; set; } = new List<string>();
-        public required string Role { get; set; }
+        public required string ProjectEffortType { get; set; }
         public required string Description { get; set; }
         public string? CodeLink { get; set; }
-        public string? Image { get; set;  }
+        public string? Image { get; set; }
+    }
+
+    public enum ProjectEffortType
+    {
+        Solo,
+        Team
     }
 }
