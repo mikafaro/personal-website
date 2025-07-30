54584722aaef25b3fba6cb90b42332c7322f2c87
Mikael Færøvik <mikael.farovik@gmail.com>
2025-07-30 16:48:54 +0200

    Add Project Card

---
+++ personal-website-blazor/Models/Project.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace PersonalWebsiteBlazor.Models
+{
+    public class Project
+    {
+        public required string Name { get; set; }
+        public required int Year { get; set; }
+        public required List<string> Technologies { get; set; } = new List<string>();
+        public required string Role { get; set; }
+        public required string Description { get; set; }
+        public string? CodeLink { get; set; }
+        public string? Image { get; set;  }
+    }
+}
